cbc6ecbc164171d4bf978e3a59e9d9d0712ca83f
treasurejeffy <treasurejeffy@gmail.com>
2026-02-23 16:29:08 +0100

    changes and updating to push to live

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.njvg83js194"
+    "revision": "0.5ti3fs16ts4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
